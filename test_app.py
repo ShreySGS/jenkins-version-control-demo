5c7b1cd7cedfed72f7655fc228431fde7982304e
Shreyas G S <108188821+ShreySGS@users.noreply.github.com>
2025-02-04 19:24:27 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import hello_world, add_numbers
+
+def test_hello_world():
+    assert hello_world() == "Hello from Version Control Jenkins Pipeline!"
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
