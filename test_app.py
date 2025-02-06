304d9495f09b2dc5d778d6ea04597d3dca4d3396
Shreyas G S <108188821+ShreySGS@users.noreply.github.com>
2025-02-06 11:12:01 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from app import hello_world, add_numbers, multiply_numbers
+from app import hello_world, add_numbers
 
 def test_hello_world():
     assert hello_world() == "Hello from Version Control Jenkins Pipeline!"
@@ -6,5 +6,3 @@ def test_hello_world():
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
 
-def test_multiply_numbers():
-    assert multiply_numbers(2, 3) == 6
